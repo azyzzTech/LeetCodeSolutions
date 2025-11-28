b554e4932c781e95d1abf406422d5a040fc44aef
azyzzTech <abdulaziz.sudo@gmail.com>
2025-11-28 14:16:03 +0300

    IntToRoman Solution add

---
+++ LeetCodeSolutions.Core/Problems/Medium/IntToRomanSolution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCodeSolutions.Core.Problems.Medium;
+public class IntToRomanSolution
+{
+    public static string IntToRoman(int num)
+    {
+        string[] M = { "", "M", "MM", "MMM" };
+        string[] C = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+        string[] X = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+        string[] I = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+        return M[num / 1000] +
+               C[(num % 1000) / 100] +
+               X[(num % 100) / 10] +
+               I[num % 10];
+    }
+}
