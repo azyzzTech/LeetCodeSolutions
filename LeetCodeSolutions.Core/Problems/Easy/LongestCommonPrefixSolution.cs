63ab4cc53d24ff89f71675a58efa71971dfce4f3
azyzzTech <abdulaziz.sudo@gmail.com>
2025-12-02 18:59:27 +0300

    LongestCommonPrefix Add

---
+++ LeetCodeSolutions.Core/Problems/Easy/LongestCommonPrefixSolution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeSolutions.Core.Problems.Easy;
+public static class LongestCommonPrefixSolution
+{
+    public static string LongestCommonPrefix(string[] strs)
+    {
+        string firstWord = strs[0];
+        int lengthOfFirst = firstWord.Length;
+
+        for (int i = 0; i < lengthOfFirst; i++)
+        {
+            char currentChar = firstWord[i];
+
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (i == strs[j].Length || strs[j][i] != currentChar)
+                {
+                    return firstWord.Substring(0, i);
+                }
+            }
+        }
+
+        return firstWord;
+    }
+}
