11db2e98bbc678a739aa3dae0bb5c0f72aafe99f
azyzzTech <abdulaziz.sudo@gmail.com>
2025-12-09 13:40:17 +0300

    RemoveDuplicatesSolution Added

---
+++ LeetCodeSolutions.Core/Problems/Easy/RemoveDuplicatesSolution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeSolutions.Core.Problems.Easy;
+
+public class RemoveDuplicatesSolution
+{
+    public static int RemoveDuplicates(int[] nums)
+    {
+        int k = 1;
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
