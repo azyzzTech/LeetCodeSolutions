5fe403317efe2db507d007fb8d3ad4b09b1f1ba0
azyzzTech <abdulaziz.sudo@gmail.com>
2025-12-10 15:38:23 +0300

    DivideSolution Added

---
+++ LeetCodeSolutions.Core/Problems/Medium/DivideSolution.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeSolutions.Core.Problems.Medium;
+
+public class DivideSolution
+{
+    public static int Divide(int dividend, int divisor)
+    {
+        if (dividend == int.MinValue && divisor == -1)
+        {
+            return int.MaxValue;
+        }
+
+        bool isNegative = (dividend < 0) ^ (divisor < 0);
+
+        long absDividend = Math.Abs((long)dividend);
+        long absDivisor = Math.Abs((long)divisor);
+
+        int result = 0;
+
+        while (absDividend >= absDivisor)
+        {
+            long tempDivisor = absDivisor;
+            long multiple = 1;
+
+            while (absDividend >= (tempDivisor << 1))
+            {
+                tempDivisor <<= 1;
+                multiple <<= 1;
+            }
+
+            absDividend -= tempDivisor;
+            result += (int)multiple;
+        }
+
+        return isNegative ? -result : result;
+    }
+}
