9f60382515316d77955efbb7cb0415a404069e6f
azyzzTech <abdulaziz.sudo@gmail.com>
2025-12-11 18:38:57 +0300

    RemoveElementSolution Added

---
+++ LeetCodeSolutions.Core/Problems/Easy/RemoveElementSolution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeSolutions.Core.Problems.Easy;
+
+public class RemoveElementSolution
+{
+    public static int RemoveElement(int[] nums, int val)
+    {
+        int k = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != val)
+            {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
