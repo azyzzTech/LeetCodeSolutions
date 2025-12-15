6b79f60778d086304d75d8402bf772a07af75c12
azyzzTech <abdulaziz.sudo@gmail.com>
2025-12-15 15:00:58 +0300

    LengthOfLastWordSolution Added

---
+++ LeetCodeSolutions.Core/Problems/Easy/LengthOfLastWordSolution.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCodeSolutions.Core.Problems.Easy;
+
+public class LengthOfLastWordSolution
+{
+    public static int LengthOfLastWord(string s)
+    {
+        return s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Last().Length;
+    }
+}
