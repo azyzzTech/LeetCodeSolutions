731d343660e992374c16840477f36c076f156b0c
azyzzTech <abdulaziz.sudo@gmail.com>
2025-12-16 15:28:59 +0300

    HaystackNeedleSolution Added

---
+++ LeetCodeSolutions.Core/Problems/Easy/HaystackNeedleSolution.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCodeSolutions.Core.Problems.Easy;
+
+public class HaystackNeedleSolution
+{
+    public static int StrStr(string haystack, string needle)
+    {
+        return haystack.IndexOf(needle, 0, StringComparison.Ordinal);
+    }
+}
