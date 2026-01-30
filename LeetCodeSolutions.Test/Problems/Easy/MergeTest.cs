0de2957c7a0820ca789a193380b6f95ddf2ef805
azyzzTech <abdulaziz.sudo@gmail.com>
2026-01-30 23:58:58 +0300

    Merge_MergesArraysCorrectly Added

--- LeetCodeSolutions.Test/Problems/Easy/MergeTest.cs
+++ LeetCodeSolutions.Test/Problems/Easy/MergeTest.cs
@@ -25,9 +25,20 @@
 /// Space Complexity: $O(1)$ - No extra space is used as we modify nums1 in-place.
 /// </remarks>
 
-
 namespace LeetCodeSolutions.Test.Problems.Easy;
 
 public class MergeTest
 {
+    [Theory]
+    [InlineData(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3, new int[] { 1, 2, 2, 3, 5, 6 })]
+    [InlineData(new int[] { 1 }, 1, new int[] { }, 0, new int[] { 1 })]
+    [InlineData(new int[] { 0 }, 0, new int[] { 1 }, 1, new int[] { 1 })]
+    [InlineData(new int[] { 2, 0 }, 1, new int[] { 1 }, 1, new int[] { 1, 2 })]
+    public void Merge_MergesArraysCorrectly(int[] nums1, int m, int[] nums2, int n, int[] expected)
+    {
+        var nums1Copy = (int[])nums1.Clone();
+        var solution = new Core.Problems.Easy.MergeSolution();
+        solution.Merge(nums1Copy, m, nums2, n);
+        Assert.Equal(expected, nums1Copy);
+    }
 }
