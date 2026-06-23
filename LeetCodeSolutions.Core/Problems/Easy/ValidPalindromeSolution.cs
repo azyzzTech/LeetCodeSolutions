1b166078cbac5671b513661de92149c206099776
azyzzTech <abdulaziz.sudo@gmail.com>
2026-06-23 21:58:35 +0300

    Add ValidPalindrome solution and unit tests
    Implemented ValidPalindromeSolution with IsPalindrome method to check for valid palindromes, ignoring non-alphanumeric characters and case. Added ValidPalindromeTest using xUnit to cover various input scenarios.

---
+++ LeetCodeSolutions.Core/Problems/Easy/ValidPalindromeSolution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeSolutions.Core.Problems.Easy;
+
+public class ValidPalindromeSolution
+{
+    public bool IsPalindrome(string s)
+    {
+        if (s is null) return false;
+
+        int left = 0, right = s.Length - 1;
+        while (left < right)
+        {
+            while (left < right && !char.IsLetterOrDigit(s[left])) left++;
+            while (left < right && !char.IsLetterOrDigit(s[right])) right--;
+
+            if (char.ToLowerInvariant(s[left]) != char.ToLowerInvariant(s[right]))
+                return false;
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
---
+++ LeetCodeSolutions.Test/Problems/Easy/ValidPalindromeTest.cs
@@ -0,0 +1,20 @@
+﻿using LeetCodeSolutions.Core.Problems.Easy;
+
+namespace LeetCodeSolutions.Test.Problems.Easy;
+
+public class ValidPalindromeTest
+{
+    [Theory]
+    [InlineData("A man, a plan, a canal: Panama", true)]
+    [InlineData("race a car", false)]
+    [InlineData("", true)]
+    [InlineData(" ", true)]
+    [InlineData("0P", false)]
+    [InlineData("ab_a", true)]
+    public void IsPalindrome_VariousInputs_ReturnsExpected(string input, bool expected)
+    {
+        var sut = new ValidPalindromeSolution();
+        var result = sut.IsPalindrome(input);
+        Assert.Equal(expected, result);
+    }
+}
