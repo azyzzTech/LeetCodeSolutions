b0a98fd67accb9cfdb67ba82c5f94238702d19ec
azyzzTech <abdulaziz.sudo@gmail.com>
2026-06-29 12:02:20 +0300

    Add CountingBits solution and unit tests
    Implemented CountingBitsSolution using dynamic programming to count 1-bits for numbers 0 to n. Added CountingBitsTest with parameterized test cases to validate the solution.

---
+++ LeetCodeSolutions.Core/Problems/Easy/CountingBitsSolution.cs
@@ -0,0 +1,19 @@
+﻿//https://leetcode.com/problems/counting-bits/
+
+namespace LeetCodeSolutions.Core.Problems.Easy;
+
+public class CountingBitsSolution
+{
+    public int[] CountBits(int n)
+    {
+        int[] ans = new int[n + 1];
+        ans[0] = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            ans[i] = ans[i >> 1] + (i & 1);
+        }
+
+        return ans;
+    }
+}
---
+++ LeetCodeSolutions.Test/Problems/Easy/CountingBitsTest.cs
@@ -0,0 +1,17 @@
+﻿using LeetCodeSolutions.Core.Problems.Easy;
+
+namespace LeetCodeSolutions.Test.Problems.Easy;
+
+public class CountingBitsTest
+{
+    [Theory]
+    [InlineData(0, new int[] { 0 })]
+    [InlineData(2, new int[] { 0, 1, 1 })]
+    [InlineData(5, new int[] { 0, 1, 1, 2, 1, 2 })]
+    public void CountBits_ReturnsExpectedResult(int n, int[] expected)
+    {
+        var solution = new CountingBitsSolution();
+        var result = solution.CountBits(n);
+        Assert.Equal(expected, result);
+    }
+}
